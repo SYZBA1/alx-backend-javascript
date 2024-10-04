baceff05d33465e8a9a8b30158ffda8fbf3bb26c
SYZBA1 <samyos9090@gmail.com>
2024-10-04 21:42:55 +0000

    update

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,6 +1,9 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  const hood1989 = (expansion1989 === undefined) ? 89 : expansion1989; // Local variable for 1989
-  const hood2019 = (expansion2019 === undefined) ? 19 : expansion2019; // Local variable for 2019
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  return initialNumber + hood1989 + hood2019;
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
