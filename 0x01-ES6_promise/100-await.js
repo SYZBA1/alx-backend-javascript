63aa974a0d501c8fb3afed133a21c884743ff799
SYZBA1 <samyos9090@gmail.com>
2024-10-08 11:26:25 +0000

    fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,19 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
-  const obj = {}
+  let res = {};
 
-  await Promise.all([photoPromise, userPromise])
-    .then((values) => {
-      obj.photo = values[0]
-      obj.user = values[1]
-    })
-    .catch(() => {
-      obj.photo = null
-      obj.user = null
-    })
-
-  return obj;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
