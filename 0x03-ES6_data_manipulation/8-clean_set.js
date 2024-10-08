423da5c8f5c536d21f023f6ffbca91fe507beea2
SYZBA1 <samyos9090@gmail.com>
2024-10-08 21:30:46 +0000

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,13 @@
-export default function cleanSet(set, startString) {
-  const newString = [];
-  const len = startString.length;
+const cleanSet = (set, startString) => {
+  const strings = [];
 
-  if (
-    typeof set !== 'object'
-    || typeof startString !== 'string'
-    || startString.length === 0
-  ) {
-    return '';
-  }
-
-  for (const word of set) {
-    // check if word starts with startString
-    if (word && word.startsWith(startString)) {
-      newString.push(word.slice(len));
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
     }
-  }
+  });
+  return strings.join('-');
+};
 
-  return newString.join('-');
-}
+export default cleanSet;
